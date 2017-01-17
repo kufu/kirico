725ce857287d72a4ad9eb03fbac783edee826cca
kakipo <kakipo@gmail.com>
2017-01-17 20:06:51 +0900

    住所変更レコードの実装

--- lib/kirico/models/changing_address_record.rb
+++ lib/kirico/models/changing_address_record.rb
@@ -1,16 +1,59 @@
 # frozen_string_literal: true
 require 'virtus'
+require 'kirico/models/helper'
 
 module Kirico
   class ChangingAddressRecord
     include Virtus.model
+    extend Kirico::Helper
+
+    DOC_CODE = '22187041'
+
+    attribute :area_code, String
+    attribute :office_code, String
+    attribute :ip_code, String
+    attribute :basic_pension_number1, String
+    attribute :basic_pension_number2, String
+    attribute :birth_at_era_code, String
+    attribute :birth_at, String
+    attribute :zip_code1, String
+    attribute :zip_code2, String
+    attribute :new_address_yomi, String
+    attribute :new_address, String
+    attribute :updated_at_era_code, String
+    attribute :updated_at, String
+    attribute :ip_name_yomi, String
+    attribute :ip_name, String
+    attribute :old_address_yomi, String
+    attribute :old_address, String
+    attribute :memo, String
 
     def initialize
-      super
+      yield(self) if block_given?
     end
 
     def to_csv
-      CSV.table('spec/fixtures/changing_address_SHFD0006.CSV', encoding: 'Shift_JIS:UTF-8', headers: false, converters: nil)[5].join(',')
+      [
+        DOC_CODE,
+        area_code,
+        office_code,
+        ip_code,
+        basic_pension_number1,
+        basic_pension_number2,
+        birth_at_era_code,
+        birth_at,
+        zip_code1,
+        zip_code2,
+        new_address_yomi,
+        new_address,
+        updated_at_era_code,
+        updated_at,
+        ip_name_yomi,
+        ip_name,
+        old_address_yomi,
+        old_address,
+        memo
+      ].join(',')
     end
   end
 end
