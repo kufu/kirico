68130c67e76d3fd2305406cf6c5e31e290d0a9c0
wakasa51 <y.takashi51@gmail.com>
2019-06-04 18:28:10 +0900

    FactoryBotで静的属性が使えなくなったのでFactoryを修正します。

--- spec/factories/company.rb
+++ spec/factories/company.rb
@@ -2,14 +2,14 @@
 
 FactoryBot.define do
   factory :company, class: Kirico::Company do
-    area_code '14'
-    office_code 'ｸﾄﾜ'
-    office_number '77362'
-    zip_code1 '106'
-    zip_code2 '0041'
-    address '東京都港区麻布台1-4-3 エグゼクティブタワー麻布台601'
-    name '株式会社クフ'
-    owner_name '吉田　ケイ　松蔭'
-    tel_number '03-5563-7662'
+    area_code { '14' }
+    office_code { 'ｸﾄﾜ' }
+    office_number { '77362' }
+    zip_code1 { '106' }
+    zip_code2 { '0041' }
+    address { '東京都港区麻布台1-4-3 エグゼクティブタワー麻布台601' }
+    name { '株式会社クフ' }
+    owner_name { '吉田　ケイ　松蔭' }
+    tel_number { '03-5563-7662' }
   end
 end
--- spec/factories/fd_management_record.rb
+++ spec/factories/fd_management_record.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :fd_management_record, class: Kirico::FDManagementRecord do
-    area_code '14'
-    office_code 'ｸﾄﾜ'
-    fd_seq_number '005'
-    created_at Date.new(2017, 1, 17)
+    area_code { '14' }
+    office_code { 'ｸﾄﾜ' }
+    fd_seq_number { '005' }
+    created_at { Date.new(2017, 1, 17) }
   end
 end
